80c40320cae1101b61eb37fb3ba700623c9486e1
andywillmot <andy@thewillmots.co.uk>
2015-03-24 09:17:00 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -5,7 +5,7 @@ from crispy_forms.layout import Layout, ButtonHolder, Submit
 
 class DeliverableListFormHelper(FormHelper):
     model = Deliverable
-    form_tag = True
+    form_tag = False
     attrs = {"role": "form"}
 
 class ReviewIterationListFormHelper(FormHelper):
