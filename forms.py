9076d901dd6949cc3445c338bbbd68c5eb5bd52d
andywillmot <andy@thewillmots.co.uk>
2015-03-24 19:58:48 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,25 +4,25 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, ButtonHolder, Submit
 from django.core.urlresolvers import reverse
 
-class DeliverableListFormHelper(FormHelper):
-    model = Deliverable
-    form_tag = False
+#class DeliverableListFormHelper(FormHelper):
+#    model = Deliverable
+#    form_tag = False
 
-class ReviewIterationListFormHelper(FormHelper):
-    model = ReviewIteration
-    form_tag = True
-    attrs = {"role": "form"}
+#class ReviewIterationListFormHelper(FormHelper):
+#    model = ReviewIteration
+#    form_tag = True
+#    attrs = {"role": "form"}
 #    layout = Layout('pk', ButtonHolder(
 #        Submit('submit', 'Filter', css_class='btn btn-default')
 #    ))
     
-class CommentListFormHelper(FormHelper):
-    model = Comment
-    form_tag = True   
-    attrs = {"role": "form"}
-    layout = Layout('review_iteration','template_deliverable_section', ButtonHolder(
-        Submit('submit', 'Filter', css_class='btn btn-default')
-    ))
+#class CommentListFormHelper(FormHelper):
+#    model = Comment
+#    form_tag = True   
+#    attrs = {"role": "form"}
+#    layout = Layout('review_iteration','template_deliverable_section', ButtonHolder(
+#        Submit('submit', 'Filter', css_class='btn btn-default')
+#    ))
 
 class DeliverableForm(forms.ModelForm):
     class Meta:
@@ -39,14 +39,14 @@ class DeliverableForm(forms.ModelForm):
 class ReviewIterationForm(forms.ModelForm):
     class Meta:
         model = ReviewIteration
-    def __init__(self, *args, **kwargs):
-        super(ReviewIterationForm, self).__init__(*args, **kwargs)
-        self.helper = ReviewIterationFormHelper()
+#    def __init__(self, *args, **kwargs):
+#        super(ReviewIterationForm, self).__init__(*args, **kwargs)
+#        self.helper = ReviewIterationFormHelper()
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-    def __init__(self, *args, **kwargs):
-        super(CommentForm, self).__init__(*args, **kwargs)
-        self.helper = CommentFormHelper()
+ #   def __init__(self, *args, **kwargs):
+ #       super(CommentForm, self).__init__(*args, **kwargs)
+ #       self.helper = CommentFormHelper()
     
